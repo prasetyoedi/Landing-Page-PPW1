063e9abe92ecb620921a6c99446c5c1d2e231bda
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-05-30 21:29:03 +0700

    update index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,38 +2,41 @@ function sayHello() {
     alert("Welcome to The Ocean!!!");
 }
 
+var originalText = ""; // Variabel untuk menyimpan teks asli
+
 function editTeks() {
     var teksAwal = document.getElementById("teksAwal");
     var editButton = document.getElementById("editButton");
     var editArea = document.getElementById("editArea");
     var saveButton = document.getElementById("saveButton");
 
-    var teks = teksAwal.textContent; // Mendapatkan teks awal
-    editArea.value = teks; // Mengisi textarea dengan teks awal
-    teksAwal.style.display = "none"; // Menyembunyikan teks awal
-    editButton.style.display = "none"; // Menyembunyikan tombol edit
-    editArea.style.display = "block"; // Menampilkan textarea
-    saveButton.style.display = "block"; // Menampilkan tombol save
+    if (originalText === "") {
+        originalText = teksAwal.textContent; // Menyimpan teks asli hanya saat pertama kali diedit
+    }
+
+    editArea.value = originalText; // Mengisi textarea dengan teks asli atau teks yang sudah disimpan
+    teksAwal.style.display = "none";
+    editButton.style.display = "none";
+    saveButton.style.display = "block";
+    editArea.style.display = "block";
 }
 
 function simpanTeks() {
     var editArea = document.getElementById("editArea");
     var teksHasil = document.getElementById("teksHasil");
+    var saveButton = document.getElementById("saveButton");
 
-    var teks = editArea.value; // Mendapatkan teks yang diubah dalam textarea
-    teksHasil.textContent = teks; // Menampilkan teks yang diubah dalam elemen p
+    var teks = editArea.value;
+    teksHasil.textContent = teks;
 
-    editArea.style.display = "none"; // Menyembunyikan textarea
-    saveButton.style.display = "none"; // Menyembunyikan tombol save
-    teksHasil.style.display = "block"; // Menampilkan teks hasil
-    // Mengosongkan textarea setelah disimpan
-    editArea.value = "";
+    editArea.style.display = "none";
+    saveButton.style.display = "none";
+    teksHasil.style.display = "block";
+    editButton.style.display = "block"; // Menampilkan tombol "Edit" setelah hasil disimpan
+
+    originalText = teks; // Menyimpan teks yang diubah sebagai teks asli yang akan ditampilkan ketika tombol "Edit" ditekan kembali
 }
 
-var editButton = document.getElementById("editButton");
-var editArea = document.getElementById("editArea");
-var saveButton = document.getElementById("saveButton");
-var teksHasil = document.getElementById("teksHasil");
 
 editButton.addEventListener("click", editTeks);
 editArea.cols = "35";
